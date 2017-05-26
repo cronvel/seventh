bd6d8c3029d6a8b3da530af660550b4f1831e5ee
Cédric Ronvel <cedric.ronvel@gmail.com>
2017-05-26 17:02:11 +0200

    Fixing and improving things

--- sample/promise-test.js
+++ sample/promise-test.js
@@ -37,6 +37,32 @@ var count = 0 ;
 
 
 
+function normal( txt , delay = 250 )
+{
+	return new Promise( ( resolve , reject ) => {
+		setTimeout( () => {
+			console.log( txt ) ;
+			resolve( txt ) ;
+		} , delay ) ;
+	} ) ;
+}
+
+var p = normal( 'one' ) ;
+p.then( ( value ) => {
+	console.log( 'then two' , value ) ;
+	return normal( 'two' , 1500 ) ;
+} )
+.then( ( value ) => {
+	console.log( 'then three' , value ) ;
+	return normal( 'three' ) ;
+} )
+.then( ( value ) => {
+	console.log( 'then four' , value ) ;
+	return normal( 'four' ) ;
+} )
+
+return ;
+
 function recursive()
 {
 	return new Promise( ( resolve , reject ) => {
